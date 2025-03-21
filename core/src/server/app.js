aa05d8b1eeafcf53c8ba80c97e722c0bfc0ce720
Dean Makwarimba <dean.mak13@hotmail.com>
2025-03-21 02:20:20 +0000

    azure site working, no charts showing, likely eventgrid issue

--- core/src/server/app.js
+++ core/src/server/app.js
@@ -1,7 +1,8 @@
 const grpc_server = require('./grpc/grpc_server');
 const utils = require('./utils');
 const {RealTimeMarketData} = require("./datasource/coinbase_client");
-const args = require('minimist')(process.argv.slice(2)); 
+const args = require('minimist')(process.argv.slice(2));
+const path = require('path');
 const {ProductCandleRequest} = require("./grpc/gen/coinbase/v1/coinbase_products_pb")
 const {handleEvents} = require("./event/event_grid_subscriber");
 const express = require("express");
@@ -33,7 +34,20 @@ async function startEventGridListener(){
     app.listen(eventGridport, () => {logger.info(`EventGridSubscriber subscribed to endpoint: ${candleAnalyticsSubEndPoint}`);});
 }
 
+function serveFrontEndBuild(){
+    app.use(express.static(path.join(__dirname, '..', '..', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+    });
+    // Bind to port whatever websites port in azure is set to or 8000 default
+    const frontEndPort = process.env.WEBSITES_PORT || 8000;
+    app.listen(frontEndPort, () => {
+        console.log(`Listening on port ${frontEndPort}`);
+    });
+}
+
 (async () => {
+    serveFrontEndBuild();
     await startEventGridListener();
     realTimeProductCandlePipeline();
 })();
